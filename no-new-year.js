99a240a8d63fbd726a045ef7d52ffd38b58e5991
BOR-LAND <93826441+BOR-LAND@users.noreply.github.com>
2025-12-31 18:13:52 +0500

    Update no-new-year.js

--- no-new-year.js
+++ no-new-year.js
@@ -1,7 +1,11 @@
 (function () {
     'use strict';
-    
+
+    // Удаляем кнопку при добавлении плагина
+    // document.querySelectorAll('.new-year__button').forEach(e => e.remove());
+
     // 1. Ставим флаг, что плагин якобы уже запущен. 
     // Настоящий новогодний плагин увидит это и сразу выключится.
     window.new_year = true;
 })();
+
