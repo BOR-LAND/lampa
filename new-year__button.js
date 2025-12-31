9f28070fbe50a63a8c82e8e0845898e3f3278447
BOR-LAND <93826441+BOR-LAND@users.noreply.github.com>
2025-12-31 17:05:41 +0500

    Update new-year__button.js

--- new-year__button.js
+++ new-year__button.js
@@ -1,11 +1,15 @@
 (function () {
     'use strict';
-
+    // Слушаем событие готовности приложения
     Lampa.Listener.follow('app', function (e) {
-        if (e.type === 'ready') {
-            document
-                .querySelectorAll('.new-year__button')
-                .forEach(el => el.remove());
+        if (e.type == 'ready') {
+            // Вырезаем кнопку, как только приложение готово
+            // Добавляем небольшую задержку, чтобы Lampa успела отрисовать шапку
+            setTimeout(function() {
+                document.querySelectorAll('.new-year_button, .new-year__button').forEach(function(el) {
+                    el.remove();
+                });
+            }, 100);
         }
     });
 })();
