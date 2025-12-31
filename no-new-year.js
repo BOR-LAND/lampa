4a67b736967dae508da7bae2563b63de7db703d3
BOR-LAND <93826441+BOR-LAND@users.noreply.github.com>
2025-12-31 21:09:48 +0500

    Update no-new-year.js

--- no-new-year.js
+++ no-new-year.js
@@ -1,28 +1,10 @@
 (function () {
     'use strict';
-
-    // 1. Создаем функцию "снайпера"
-    function removeNewYearButton() {
-        var btn = document.querySelector('.new-year__button');
-        if (btn) {
-            btn.remove();
-            observer.disconnect();
-            observer = null;
-        }
-    };
-
-    // 2. Используем MutationObserver — он следит за появлением элементов в реальном времени.
-    // Это работает быстрее и надежнее любых событий Lampa.
-    var observer = new MutationObserver(function() {
-        removeNewYearButton();
-    });
-
-    // Запускаем слежку за всем документом
-    observer.observe(document.documentElement, {
-        childList: true
-    });
-
-    // 3. И на всякий случай запускаем проверку сразу при старте скрипта
-    removeNewYearButton();
-
+    
+    // Удаляем кнопку при добавлении плагина
+    // document.querySelectorAll('.new-year__button').forEach(e => e.remove());
+    
+    // Ставим флаг, что плагин якобы уже запущен. 
+    // Настоящий новогодний плагин увидит это и сразу выключится.
+    window.new_year = true;
 })();
