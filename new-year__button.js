6d61db4d5327133ca44d5ee6066710cae0705429
BOR-LAND <93826441+BOR-LAND@users.noreply.github.com>
2025-12-31 17:49:31 +0500

    Update new-year__button.js

--- new-year__button.js
+++ new-year__button.js
@@ -1,16 +1,26 @@
 (function () {
     'use strict';
     
-    // Пишем в лог сразу при старте файла
-    console.log('TEST: Скрипт запущен');
+    // 1. Ставим флаг, что плагин якобы уже запущен. 
+    // Настоящий новогодний плагин увидит это и сразу выключится.
+    window.new_year = true;
 
-    Lampa.Listener.follow('all', function (e) {
-        var btn = document.querySelector('.new-year_button, .new-year__button');
-        
-        if (btn) {
-            // Если кнопка найдена, выскочит окно с названием события
-            alert('Кнопка найдена! Событие: ' + e.event + ' | Тип: ' + e.type);
-            console.log('!!! ЛОВУШКА СРАБОТАЛА на:', e.event, e.type);
-        }
-    });
+    // 2. На случай, если он уже успел проскочить, блокируем загрузку скрипта
+    var originalPut = Lampa.Utils.putScriptAsync;
+    Lampa.Utils.putScriptAsync = function (scripts) {
+        var filtered = scripts.filter(function (src) {
+            return !src.includes('new-year') && !src.includes('snow');
+        });
+        return filtered.length > 0 ? originalPut.call(Lampa.Utils, filtered) : Promise.resolve();
+    };
+
+    // 3. Финальный удар: если кнопка всё же успела отрисоваться (мало ли)
+    var kill = function() {
+        var btn = document.querySelector('.new-year__button, .new-year_button');
+        if (btn) btn.remove();
+    };
+    new MutationObserver(kill).observe(document.documentElement, {childList: true, subtree: true});
+    kill();
+
+    console.log('Plugin: New Year Blocked Forever');
 })();
