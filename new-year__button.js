28b1bf65e700556ca3350df1952e9a59dd350641
BOR-LAND <93826441+BOR-LAND@users.noreply.github.com>
2025-12-31 16:46:55 +0500

    Update new-year__button.js

--- new-year__button.js
+++ new-year__button.js
@@ -1,18 +1,4 @@
 (function () {
     'use strict';
-    
-    // Создаем стиль, который скрывает элемент
-    var css = '.new-year_button { display: none !important; }';
-    
-    // Добавляем этот стиль на страницу
-    var style = document.createElement('style');
-    style.type = 'text/css';
-    if (style.styleSheet){
-      style.styleSheet.cssText = css;
-    } else {
-      style.appendChild(document.createTextNode(css));
-    }
-    document.head.appendChild(style);
-    
-    console.log('Plugin: New Year button hidden');
+    document.querySelectorAll('.new-year__button').forEach(e => e.remove());
 })();
